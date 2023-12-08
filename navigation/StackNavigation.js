c4ee3efc3f2e1cdbf7cfc51b855d26ee3106c105
quang <quanglatoi002@gmail.com>
2023-12-08 14:17:11 +0700

    build navigation

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+
+const StackNavigation = () => {
+    const Stack = createNativeStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default StackNavigation;
+
+const styles = StyleSheet.create({});
