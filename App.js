ea55c85c9bccbd3c4be2897e17c3da3f727dccdd
quang <quanglatoi002@gmail.com>
2023-12-09 16:46:33 +0700

    build redux-tookit vs axios

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigation from "./navigation/StackNavigation";
-
+import { Provider } from "react-redux";
+import { ModalPortal } from "react-native-modals";
+import store from "./store";
+import { UserContext } from "./userContext";
 export default function App() {
     return (
         <>
-            <StackNavigation />
+            <Provider store={store}>
+                <UserContext>
+                    <StackNavigation />
+                    <ModalPortal />
+                </UserContext>
+            </Provider>
         </>
     );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,7 +9,8 @@ import {
     Image,
 } from "react-native";
 import { SliderBox } from "react-native-image-slider-box";
-import React, { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker";
+import React, { useState, useCallback, useEffect } from "react";
 import {
     MaterialIcons,
     AntDesign,
@@ -17,108 +18,43 @@ import {
     Ionicons,
 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-
-const list = [
-    {
-        id: "0",
-        image: "https://m.media-amazon.com/images/I/41EcYoIZhIL._AC_SY400_.jpg",
-        name: "Home",
-    },
-    {
-        id: "1",
-        image: require("../assets/opp-4.jpg"),
-        name: "Opp",
-    },
-    {
-        id: "3",
-        image: "https://images-eu.ssl-images-amazon.com/images/I/31dXEvtxidL._AC_SX368_.jpg",
-        name: "Electronics",
-    },
-    {
-        id: "4",
-        image: require("../assets/ap15.jpg"),
-        name: "Mobiles",
-    },
-    {
-        id: "5",
-        image: "https://m.media-amazon.com/images/G/31/img20/Events/Jup21dealsgrid/music.jpg",
-        name: "Music",
-    },
-    {
-        id: "6",
-        image: "https://m.media-amazon.com/images/I/51dZ19miAbL._AC_SY350_.jpg",
-        name: "Fashion",
-    },
-];
-
-const images = [
-    "https://img.etimg.com/thumb/msid-93051525,width-1070,height-580,imgsize-2243475,overlay-economictimes/photo.jpg",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/PD23/Launches/Updated_ingress1242x550_3.gif",
-    "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Books/BB/JULY/1242x550_Header-BB-Jul23.jpg",
-];
-
-const deals = [
-    {
-        id: "20",
-        title: "OnePlus Nord CE 3 Lite 5G (Pastel Lime, 8GB RAM, 128GB Storage)",
-        oldPrice: 25000,
-        price: 19000,
-        image: "https://images-eu.ssl-images-amazon.com/images/G/31/wireless_products/ssserene/weblab_wf/xcm_banners_2022_in_bau_wireless_dec_580x800_once3l_v2_580x800_in-en.jpg",
-        carouselImages: [
-            "https://m.media-amazon.com/images/I/61QRgOgBx0L._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/61uaJPLIdML._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/510YZx4v3wL._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/61J6s1tkwpL._SX679_.jpg",
-        ],
-        color: "Stellar Green",
-        size: "6 GB RAM 128GB Storage",
-    },
-    {
-        id: "30",
-        title: "Samsung Galaxy S20 FE 5G (Cloud Navy, 8GB RAM, 128GB Storage) with No Cost EMI & Additional Exchange Offers",
-        oldPrice: 74000,
-        price: 26000,
-        image: "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/Samsung/SamsungBAU/S20FE/GW/June23/BAU-27thJune/xcm_banners_2022_in_bau_wireless_dec_s20fe-rv51_580x800_in-en.jpg",
-        carouselImages: [
-            "https://m.media-amazon.com/images/I/81vDZyJQ-4L._SY879_.jpg",
-            "https://m.media-amazon.com/images/I/61vN1isnThL._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/71yzyH-ohgL._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/61vN1isnThL._SX679_.jpg",
-        ],
-        color: "Cloud Navy",
-        size: "8 GB RAM 128GB Storage",
-    },
-    {
-        id: "40",
-        title: "Samsung Galaxy M14 5G (ICY Silver, 4GB, 128GB Storage) | 50MP Triple Cam | 6000 mAh Battery | 5nm Octa-Core Processor | Android 13 | Without Charger",
-        oldPrice: 16000,
-        price: 14000,
-        image: "https://images-eu.ssl-images-amazon.com/images/G/31/img23/Wireless/Samsung/CatPage/Tiles/June/xcm_banners_m14_5g_rv1_580x800_in-en.jpg",
-        carouselImages: [
-            "https://m.media-amazon.com/images/I/817WWpaFo1L._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/81KkF-GngHL._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/61IrdBaOhbL._SX679_.jpg",
-        ],
-        color: "Icy Silver",
-        size: "6 GB RAM 64GB Storage",
-    },
-    {
-        id: "40",
-        title: "realme narzo N55 (Prime Blue, 4GB+64GB) 33W Segment Fastest Charging | Super High-res 64MP Primary AI Camera",
-        oldPrice: 12999,
-        price: 10999,
-        image: "https://images-eu.ssl-images-amazon.com/images/G/31/tiyesum/N55/June/xcm_banners_2022_in_bau_wireless_dec_580x800_v1-n55-marchv2-mayv3-v4_580x800_in-en.jpg",
-        carouselImages: [
-            "https://m.media-amazon.com/images/I/41Iyj5moShL._SX300_SY300_QL70_FMwebp_.jpg",
-            "https://m.media-amazon.com/images/I/61og60CnGlL._SX679_.jpg",
-            "https://m.media-amazon.com/images/I/61twx1OjYdL._SX679_.jpg",
-        ],
-    },
-];
+import { deals, images, list, offers } from "../utils/contain";
+import axios from "axios";
+import ProductItem from "../components/ProductItem";
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
     const navigation = useNavigation();
     const [selectedAddress, setSelectedAdress] = useState("");
+    const [open, setOpen] = useState(false);
+    const [companyOpen, setCompanyOpen] = useState(false);
+    const [category, setCategory] = useState("jewelery");
+
+    const [items, setItems] = useState([
+        { label: "Men's clothing", value: "men's clothing" },
+        { label: "jewelery", value: "jewelery" },
+        { label: "electronics", value: "electronics" },
+        { label: "women's clothing", value: "women's clothing" },
+    ]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    "https://fakestoreapi.com/products"
+                );
+                setProducts(response.data);
+            } catch (error) {
+                console.log("error message", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const onGenderOpen = useCallback(() => {
+        setCompanyOpen(false);
+    }, []);
     return (
         <>
             <SafeAreaView
@@ -307,6 +243,115 @@ const HomeScreen = () => {
                     >
                         Today's Deals
                     </Text>
+                    <ScrollView
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        {offers.map((item, index) => (
+                            <Pressable
+                                key={index}
+                                onPress={() =>
+                                    navigation.navigate("Info", {
+                                        id: item.id,
+                                        title: item.title,
+                                        price: item?.price,
+                                        carouselImages: item.carouselImages,
+                                        color: item?.color,
+                                        size: item?.size,
+                                        oldPrice: item?.oldPrice,
+                                        item: item,
+                                    })
+                                }
+                                style={{
+                                    marginVertical: 10,
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                <Image
+                                    style={{
+                                        width: 150,
+                                        height: 150,
+                                        resizeMode: "contain",
+                                    }}
+                                    source={{ uri: item?.image }}
+                                />
+
+                                <View
+                                    style={{
+                                        backgroundColor: "#E31837",
+                                        paddingVertical: 5,
+                                        width: 130,
+                                        justifyContent: "center",
+                                        alignItems: "center",
+                                        marginTop: 10,
+                                        borderRadius: 4,
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            textAlign: "center",
+                                            color: "white",
+                                            fontSize: 13,
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        Upto {item?.offer}
+                                    </Text>
+                                </View>
+                            </Pressable>
+                        ))}
+                    </ScrollView>
+                    <Text
+                        style={{
+                            height: 1,
+                            borderColor: "#D0D0D0",
+                            borderWidth: 2,
+                            marginTop: 15,
+                        }}
+                    />
+
+                    <View
+                        style={{
+                            marginHorizontal: 10,
+                            marginTop: 20,
+                            width: "45%",
+                            marginBottom: open ? 50 : 15,
+                        }}
+                    >
+                        <DropDownPicker
+                            style={{
+                                borderColor: "#B7B7B7",
+                                height: 30,
+                                marginBottom: open ? 120 : 15,
+                            }}
+                            open={open}
+                            value={category} //genderValue
+                            items={items}
+                            setOpen={setOpen}
+                            setValue={setCategory}
+                            setItems={setItems}
+                            placeholder="choose category"
+                            placeholderStyle={styles.placeholderStyles}
+                            onOpen={onGenderOpen}
+                            // onChangeValue={onChange}
+                            zIndex={3000}
+                            zIndexInverse={1000}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            flexWrap: "wrap",
+                        }}
+                    >
+                        {products
+                            ?.filter((item) => item.category === category)
+                            .map((item, index) => (
+                                <ProductItem item={item} key={index} />
+                            ))}
+                    </View>
                 </ScrollView>
             </SafeAreaView>
         </>
