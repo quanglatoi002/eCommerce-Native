77a7441c30634a08bf8d90db1307327413fcab9f
quang <quanglatoi002@gmail.com>
2023-12-09 17:06:42 +0700

    api address

--- api/index.js
+++ api/index.js
@@ -155,3 +155,42 @@ app.post("/login", async (req, res) => {
         res.status(500).json({ message: "Login Failed" });
     }
 });
+// add address
+app.post("/addresses", async (req, res) => {
+    try {
+        const { userId, address } = req.body;
+
+        //find the user by the Userid
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        //add the new address to the user's addresses array
+        user.addresses.push(address);
+
+        //save the updated user in te backend
+        await user.save();
+
+        res.status(200).json({ message: "Address created Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Error addding address" });
+    }
+});
+
+// get add
+app.get("/addresses/:userId", async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const addresses = user.addresses;
+        res.status(200).json({ addresses });
+    } catch (error) {
+        res.status(500).json({ message: "Error retrieveing the addresses" });
+    }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,13 +21,16 @@ import { useNavigation } from "@react-navigation/native";
 import { deals, images, list, offers } from "../utils/contain";
 import axios from "axios";
 import ProductItem from "../components/ProductItem";
+import { BottomModal, SlideAnimation, ModalContent } from "react-native-modals";
 
 const HomeScreen = () => {
+    const [addresses, setAddresses] = useState([]);
     const [products, setProducts] = useState([]);
     const navigation = useNavigation();
     const [selectedAddress, setSelectedAdress] = useState("");
     const [open, setOpen] = useState(false);
     const [companyOpen, setCompanyOpen] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
     const [category, setCategory] = useState("jewelery");
 
     const [items, setItems] = useState([
@@ -36,6 +39,25 @@ const HomeScreen = () => {
         { label: "electronics", value: "electronics" },
         { label: "women's clothing", value: "women's clothing" },
     ]);
+
+    //address
+    useEffect(() => {
+        if (userId) {
+            fetchAddresses();
+        }
+    }, [userId, modalVisible]);
+    const fetchAddresses = async () => {
+        try {
+            const response = await axios.get(
+                `http://localhost:8000/addresses/${userId}`
+            );
+            const { addresses } = response.data;
+
+            setAddresses(addresses);
+        } catch (error) {
+            console.log("error", error);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -354,6 +376,147 @@ const HomeScreen = () => {
                     </View>
                 </ScrollView>
             </SafeAreaView>
+
+            <BottomModal
+                onBackdropPress={() => setModalVisible(!modalVisible)}
+                swipeDirection={["up", "down"]}
+                swipeThreshold={200}
+                modalAnimation={
+                    new SlideAnimation({
+                        slideFrom: "bottom",
+                    })
+                }
+                onHardwareBackPress={() => setModalVisible(!modalVisible)}
+                visible={modalVisible}
+                onTouchOutside={() => setModalVisible(!modalVisible)}
+            >
+                <ModalContent style={{ width: "100%", height: 400 }}>
+                    <View style={{ marginBottom: 8 }}>
+                        <Text style={{ fontSize: 16, fontWeight: "500" }}>
+                            Choose your Location
+                        </Text>
+
+                        <Text
+                            style={{
+                                marginTop: 5,
+                                fontSize: 16,
+                                color: "gray",
+                            }}
+                        >
+                            Select a delivery location to see product
+                            availabilty and delivery options
+                        </Text>
+                    </View>
+                    <ScrollView
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        {/* already added addresses */}
+                        {addresses?.map((item, index) => (
+                            <Pressable
+                                onPress={() => setSelectedAdress(item)}
+                                style={{
+                                    width: 140,
+                                    height: 140,
+                                    borderColor: "#D0D0D0",
+                                    borderWidth: 1,
+                                    padding: 10,
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    gap: 3,
+                                    marginRight: 15,
+                                    marginTop: 10,
+                                    backgroundColor:
+                                        selectedAddress === item
+                                            ? "#FBCEB1"
+                                            : "white",
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        flexDirection: "row",
+                                        alignItems: "center",
+                                        gap: 3,
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            fontSize: 13,
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {item?.name}
+                                    </Text>
+                                    <Entypo
+                                        name="location-pin"
+                                        size={24}
+                                        color="red"
+                                    />
+                                </View>
+
+                                <Text
+                                    numberOfLines={1}
+                                    style={{
+                                        width: 130,
+                                        fontSize: 13,
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    {item?.houseNo},{item?.landmark}
+                                </Text>
+
+                                <Text
+                                    numberOfLines={1}
+                                    style={{
+                                        width: 130,
+                                        fontSize: 13,
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    {item?.street}
+                                </Text>
+                                <Text
+                                    numberOfLines={1}
+                                    style={{
+                                        width: 130,
+                                        fontSize: 13,
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    India, Bangalore
+                                </Text>
+                            </Pressable>
+                        ))}
+
+                        <Pressable
+                            onPress={() => {
+                                setModalVisible(false);
+                                navigation.navigate("Address");
+                            }}
+                            style={{
+                                width: 140,
+                                height: 140,
+                                borderColor: "#D0D0D0",
+                                marginTop: 10,
+                                borderWidth: 1,
+                                padding: 10,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    textAlign: "center",
+                                    color: "#0066b2",
+                                    fontWeight: "500",
+                                }}
+                            >
+                                Add an Address or pick-up point
+                            </Text>
+                        </Pressable>
+                    </ScrollView>
+                </ModalContent>
+            </BottomModal>
         </>
     );
 };
